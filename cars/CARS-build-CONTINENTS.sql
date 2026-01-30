e5032dd92f2135cbeb8c192b2a6fe53d86b21770
nathanmanesh <155933154+nathanmanesh@users.noreply.github.com>
2026-01-30 09:41:47 -0800

    create individual insert statements

--- cars/CARS-build-CONTINENTS.sql
+++ cars/CARS-build-CONTINENTS.sql
@@ -1,6 +1,6 @@
-INSERT INTO CONTINENTS (ContId, Continent) VALUES
-(1, 'america'),
-(2, 'europe'),
-(3, 'asia'),
-(4, 'africa'),
-(5, 'australia');
+INSERT INTO CONTINENTS (ContId, Continent) VALUES (1, 'america');
+INSERT INTO CONTINENTS (ContId, Continent) VALUES (2, 'europe');
+INSERT INTO CONTINENTS (ContId, Continent) VALUES (3, 'asia');
+INSERT INTO CONTINENTS (ContId, Continent) VALUES (4, 'africa');
+INSERT INTO CONTINENTS (ContId, Continent) VALUES (5, 'australia');
+
